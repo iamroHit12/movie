955d8e89b79ac09170e11e9513aaa97c8b5612a8
Rohit Prasad <72003474+iamroHit12@users.noreply.github.com>
2021-08-30 21:00:27 +0530

    Add files via upload

---
+++ Profile.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $('.target').hide();
+    $('#div1').show();
+});
---
+++ read.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $('.target').hide();
+    $('#div2').show();
+});
