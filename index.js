33c169628f3a978eeeec8e2ec913c7abc4fff387
Rohit Prasad <72003474+iamroHit12@users.noreply.github.com>
2021-09-06 00:49:50 +0530

    moviecard ui changes

--- index.js
+++ index.js
@@ -21,21 +21,22 @@ $(document).ready(async function(){
         <div class="moviesCard">
             <img src=${'http://image.tmdb.org/t/p/original'+`${item.poster_path}`}
                 alt="">
-            <div class="moviesCard_foot row no-gutters mt-3">
-                <div class="foot">
-                    <h3>${item.original_title}</h3>
-                    <h5>
-                        <svg width="28" height="30" viewBox="0 0 28 30" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M15 20.0875L22.725 24.75L20.675 15.9625L27.5 10.05L18.5125 9.2875L15 1L11.4875 9.2875L2.5 10.05L9.325 15.9625L7.275 24.75L15 20.0875Z"
-                                fill="#FFB800" />
-                        </svg>
-                        ${item.vote_average}
-                    </h5>
+            <div class="moviesCard_foot row no-gutters m-0">
+                <h3>${item.original_title}</h3>
+                <div class="d-flex">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h5>
+                            <svg width="28" height="30" viewBox="0 0 28 30" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M15 20.0875L22.725 24.75L20.675 15.9625L27.5 10.05L18.5125 9.2875L15 1L11.4875 9.2875L2.5 10.05L9.325 15.9625L7.275 24.75L15 20.0875Z"
+                                    fill="#FFB800" />
+                            </svg>
+                            ${item.vote_average}
+                        </h5>
+                    </div>
+                    <button class="btn foot ms-auto shadow-none"><a href=${link1}>read more</a></button>
                 </div>
-                <button class="btn foot ms-auto shadow-none"><a href=${link1}>read
-                        more</a></button>
             </div>
         </div>
     </div>`
@@ -55,21 +56,22 @@ $(document).ready(async function(){
         <div class="moviesCard">
             <img src=${'http://image.tmdb.org/t/p/original'+`${item.poster_path}`}
                 alt="">
-            <div class="moviesCard_foot row no-gutters mt-3">
-                <div class="foot">
-                    <h3>${item.original_title}</h3>
-                    <h5>
-                        <svg width="28" height="30" viewBox="0 0 28 30" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M15 20.0875L22.725 24.75L20.675 15.9625L27.5 10.05L18.5125 9.2875L15 1L11.4875 9.2875L2.5 10.05L9.325 15.9625L7.275 24.75L15 20.0875Z"
-                                fill="#FFB800" />
-                        </svg>
-                        ${item.vote_average}
-                    </h5>
+            <div class="moviesCard_foot row no-gutters m-0">
+                <h3>${item.original_title}</h3>
+                <div class="d-flex">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h5>
+                            <svg width="28" height="30" viewBox="0 0 28 30" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M15 20.0875L22.725 24.75L20.675 15.9625L27.5 10.05L18.5125 9.2875L15 1L11.4875 9.2875L2.5 10.05L9.325 15.9625L7.275 24.75L15 20.0875Z"
+                                    fill="#FFB800" />
+                            </svg>
+                            ${item.vote_average}
+                        </h5>
+                    </div>
+                    <button class="btn foot ms-auto shadow-none"><a href=${link2}>read more</a></button>
                 </div>
-                <button class="btn foot ms-auto shadow-none"><a href=${link2}>read
-                        more</a></button>
             </div>
         </div>
     </div>`
@@ -89,21 +91,22 @@ $(document).ready(async function(){
         <div class="moviesCard">
             <img src=${'http://image.tmdb.org/t/p/original'+`${item.poster_path}`}
                 alt="">
-            <div class="moviesCard_foot row no-gutters mt-3">
-                <div class="foot">
-                    <h3>${item.original_title}</h3>
-                    <h5>
-                        <svg width="28" height="30" viewBox="0 0 28 30" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M15 20.0875L22.725 24.75L20.675 15.9625L27.5 10.05L18.5125 9.2875L15 1L11.4875 9.2875L2.5 10.05L9.325 15.9625L7.275 24.75L15 20.0875Z"
-                                fill="#FFB800" />
-                        </svg>
-                        ${item.vote_average}
-                    </h5>
+            <div class="moviesCard_foot row no-gutters m-0">
+                <h3>${item.original_title}</h3>
+                <div class="d-flex">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h5>
+                            <svg width="28" height="30" viewBox="0 0 28 30" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M15 20.0875L22.725 24.75L20.675 15.9625L27.5 10.05L18.5125 9.2875L15 1L11.4875 9.2875L2.5 10.05L9.325 15.9625L7.275 24.75L15 20.0875Z"
+                                    fill="#FFB800" />
+                            </svg>
+                            ${item.vote_average}
+                        </h5>
+                    </div>
+                    <button class="btn foot ms-auto shadow-none"><a href=${link3}>read more</a></button>
                 </div>
-                <button class="btn foot ms-auto shadow-none"><a href=${link3}>read
-                        more</a></button>
             </div>
         </div>
     </div>`
